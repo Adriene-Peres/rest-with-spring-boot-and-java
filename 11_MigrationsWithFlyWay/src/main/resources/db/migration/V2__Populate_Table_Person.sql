f4ba8f21b388d3082bac299a8026b2d358f5fb06
Adriene Peres <adriene_camargo@yahoo.com.br>
2025-04-03 17:44:50 -0300

    Migrations With FlyWay

---
+++ 11_MigrationsWithFlyWay/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,11 @@
+INSERT INTO `person`(`id`, `first_name`, `last_name`, `address`, `gender`) VALUES
+(1, 'Ayrton', 'Senna', 'São Paulo', 'Male'),
+(2, 'Leonardo', 'Costa', 'Rio de Janeiro', 'Male'),
+(4, 'Maria', 'Soares', 'Minas Gerais', 'Female'),
+(5, 'José', 'Soares', 'Minas Gerais', 'Male'),
+(7, 'Marcio', 'Ferreira', 'Pará', 'Male'),
+(8, 'Carlos', 'Rodrigues', 'Minas Gerais', 'Male'),
+(9, 'Marcia', 'Rodrigues', 'Minas Gerais', 'Female'),
+(10, 'Breno', 'Souza', 'Tocantins', 'Male'),
+(11, 'Andrea', 'Martins', 'Acre', 'Female'),
+(13, 'Carolina', 'Correia', 'Lavras - MG', 'Female');
